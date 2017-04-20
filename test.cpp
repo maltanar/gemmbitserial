d97b5d461486db6abd907e4634c84a9637e91242
Yaman Umuroglu <maltanar@gmail.com>
2017-04-20 16:41:39 +0200

    also check negations in matrix vector test

--- test.cpp
+++ test.cpp
@@ -1,5 +1,6 @@
 #include <time.h>
 #include <cstdlib>
+#include <deque>
 #include "gemm-bitserial.h"
 
 using namespace std;
@@ -44,36 +45,49 @@ bool test_conversions() {
 bool test_matrix_vector() {
   cout << "Now running matrix vector multiplication tests..." << endl;
   vector<size_t> param_bits {1, 2, 3, 4, 5, 6, 7, 8};
-  vector<size_t> param_dims {16, 32, 100, 1024, 4096};
+  vector<size_t> param_dims {16, 32, 100, 256};
+  deque<bool> param_allow_neg {false, true};
   unsigned int numConfigs = 0, ok = 0, nok = 0;
   for(auto & b: param_bits) {
     for(auto & d: param_dims) {
-      uint8_t * rnd_mat = new uint8_t[d*d];
-      uint8_t * rnd_vec = new uint8_t[d];
-      ResultVector res_golden;
-      generateRandomVector(b, d, rnd_vec);
-      generateRandomVector(b, d*d, rnd_mat);
-      BitSerialVector bsv = toBitSerialVector(rnd_vec, d, b);
-      BitSerialMatrix bsm = toBitSerialMatrix(rnd_mat, d, d, b);
-      ResultVector resvec = bitSerialMatrixVector(bsm, bsv, d);
-      // manually compute golden result
-      for(unsigned int i = 0; i < d; i++) {
-        ResultElem acc = 0;
-        for(unsigned int j = 0; j < d; j++) {
-          acc += rnd_mat[i * d + j] * rnd_vec[j];
+      for(auto & matneg: param_allow_neg) {
+        for(auto & vecneg: param_allow_neg) {
+          uint8_t * rnd_mat = new uint8_t[d*d];
+          uint8_t * rnd_vec = new uint8_t[d];
+          ResultVector res_golden;
+          generateRandomVector(b, d, rnd_vec);
+          generateRandomVector(b, d*d, rnd_mat);
+          BitSerialVector bsv = toBitSerialVector(rnd_vec, d, b);
+          BitSerialMatrix bsm = toBitSerialMatrix(rnd_mat, d, d, b);
+          ResultVector resvec = bitSerialMatrixVector(bsm, bsv, d, matneg, vecneg);
+          // manually compute golden result
+          for(unsigned int i = 0; i < d; i++) {
+            ResultElem acc = 0;
+            for(unsigned int j = 0; j < d; j++) {
+              int32_t matelem_adj = rnd_mat[i * d + j];
+              int32_t vecelem_adj = rnd_vec[j];
+              if(matneg && ((matelem_adj & (1 << (b-1))) != 0) ) {
+                matelem_adj -= 1 << b;
+              }
+              if(vecneg && ((vecelem_adj & (1 << (b-1))) != 0) ) {
+                vecelem_adj -= 1 << b;
+              }
+              acc += matelem_adj * vecelem_adj;
+            }
+            res_golden.push_back(acc);
+          }
+          int res = (res_golden == resvec) ? 0 : 1;
+          delete [] rnd_vec;
+          delete [] rnd_mat;
+          if(res == 0) {
+            ok++;
+          } else {
+            nok++;
+          }
+          numConfigs++;
+          cout << "Bits = " << b << " dim = " << d << " matneg = " << matneg << " vecneg = " << vecneg << " result = " << res << endl;
         }
-        res_golden.push_back(acc);
       }
-      int res = (res_golden == resvec) ? 0 : 1;
-      delete [] rnd_vec;
-      delete [] rnd_mat;
-      if(res == 0) {
-        ok++;
-      } else {
-        nok++;
-      }
-      numConfigs++;
-      cout << "Bits = " << b << " dim = " << d << " result = " << res << endl;
     }
   }
   cout << "Matrix vector multiplication tests: " << ok << " OK, " << nok << " NOK" << endl;
--- test.sh
+++ test.sh
@@ -1,4 +1,7 @@
-#!/bin/sh
-rm test.out
+#!/bin/bash
+
+if [ -f test.out ]; then
+    rm test.out
+fi
 g++ -march=native -O3 -std=c++11 *.cpp *.c -o test.out
 ./test.out
