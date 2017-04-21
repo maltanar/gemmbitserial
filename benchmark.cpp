c7168ad2fc9cdb6d27bbb49a8984d5b11049252a
Yaman Umuroglu <maltanar@gmail.com>
2017-04-21 14:16:17 +0200

    move to own BitVector

--- benchmark.cpp
+++ benchmark.cpp
@@ -5,9 +5,9 @@
 using namespace std;
 
 int main(int argc, char const *argv[]) {
-  tellmeall();
   size_t b = 2;
   size_t d = 1024;
+  size_t reps = 100;
   uint8_t * rnd_mat = new uint8_t[d*d];
   uint8_t * rnd_vec = new uint8_t[d];
   ResultVector res_golden;
@@ -17,12 +17,14 @@ int main(int argc, char const *argv[]) {
   BitSerialMatrix bsm = toBitSerialMatrix(rnd_mat, d, d, b);
   ResultVector resvec;
   auto start = chrono::high_resolution_clock::now();
-  for(unsigned int i = 0; i < 100; i++)
+  for(unsigned int i = 0; i < reps; i++)
     resvec = bitSerialMatrixVector(bsm, bsv, d);
   auto end = chrono::high_resolution_clock::now();
-  float mscount = chrono::duration_cast<std::chrono::milliseconds>(end-start).count() / 100.0;
+  float uscount = chrono::duration_cast<std::chrono::microseconds>(end-start).count() / (float)reps;
+  float perf = 1000000 * (d*d*b*b*2*reps / uscount);
+  cout << "Time: " << uscount << " microseconds" << endl;
+  cout << "Performance: " << perf << " binary ops per second" << endl;
 
-  cout << "Time: " << mscount << " ms" << endl;
 
   delete [] rnd_mat;
   delete [] rnd_vec;
--- gemm-bitserial.h
+++ gemm-bitserial.h
@@ -1,9 +1,30 @@
+#pragma once
 #include <stdint.h>
 #include <string.h>
 #include <vector>
+#include <memory>
 #include "roaring.hh"
 
-typedef std::vector<Roaring> BitSerialVector;
+class MyBitVector {
+public:
+  MyBitVector(size_t numBits);
+
+  void add(uint64_t index);
+  void clear();
+  bool contains(uint64_t index) const;
+  uint64_t and_cardinality(const MyBitVector & rhs) const;
+
+protected:
+  std::vector<uint64_t> m_buf;
+  size_t m_numWords, m_bufWordBits;
+  uint64_t m_posShift, m_posMask;
+
+  uint64_t getWPos(uint64_t bitIndex) const;
+  uint64_t getBPos(uint64_t bitIndex) const;
+};
+
+typedef MyBitVector BitVector;
+typedef std::vector<BitVector> BitSerialVector;
 typedef std::vector<BitSerialVector> BitSerialMatrix;
 typedef int32_t ResultElem;
 typedef std::vector<ResultElem> ResultVector;
