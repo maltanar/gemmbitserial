241ef3e594a8b478bc37b771db599a37992b6ac6
Yaman Umuroglu <maltanar@gmail.com>
2017-05-19 22:05:54 +0200

    update makefiles for Android

--- jni/Android.mk
+++ jni/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 
 LOCAL_ARM_NEON := true
 LOCAL_MODULE := benchmark
-LOCAL_SRC_FILES := ../benchmark/benchmark.cpp ../bitvector.cpp ../cnpy.cpp ../convert.cpp ../gemm-bitserial.cpp ../serialize.cpp
+LOCAL_SRC_FILES := ../benchmark/benchmark.cpp
 
 include $(BUILD_EXECUTABLE)
 
@@ -12,14 +12,6 @@ include $(CLEAR_VARS)
 
 LOCAL_ARM_NEON := true
 LOCAL_MODULE := test
-LOCAL_SRC_FILES := ../test/test.cpp ../bitvector.cpp ../cnpy.cpp ../convert.cpp ../gemm-bitserial.cpp ../serialize.cpp
-
-include $(BUILD_EXECUTABLE)
-
-include $(CLEAR_VARS)
-
-LOCAL_ARM_NEON := true
-LOCAL_MODULE := example-sfc
-LOCAL_SRC_FILES := ../example-sfc/sfc.cpp ../bitvector.cpp ../cnpy.cpp ../convert.cpp ../gemm-bitserial.cpp ../serialize.cpp
+LOCAL_SRC_FILES := ../test/test.cpp
 
 include $(BUILD_EXECUTABLE)
--- jni/Application.mk
+++ jni/Application.mk
@@ -1,6 +1,5 @@
-NDK_TOOLCHAIN_VERSION :=
 APP_STL := c++_static
 APP_ABI := arm64-v8a
-APP_CPPFLAGS := -frtti -fexceptions -O3 -std=c++11 -Wall -Wextra -pedantic -Wno-unused-variable -Wno-unused-parameter -I.
+APP_CPPFLAGS := -O3 -std=c++11 -Wall -Wextra -pedantic -Wno-unused-variable -Wno-unused-parameter -I.
 APP_LDFLAGS := -L$(SYSROOT)/usr/lib -lstdc++ -latomic
 APP_PIE := true
