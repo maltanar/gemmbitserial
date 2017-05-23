a9bf14922d2edc6e3bb28f68c2642d1bde052eff
Yaman Umuroglu <maltanar@gmail.com>
2017-05-23 17:45:51 +0200

    4x2x2 tiling, -funroll-loops

--- arch-neon.hpp
+++ arch-neon.hpp
@@ -2,32 +2,39 @@
 #include <arm_neon.h>
 
 // architecture-specific implementations of primitives for ARM NEON
+// the tiling strategies are named as:
+// _tile<lhs-rows><depth-elems><rhs-rows>
 
 template <typename AccType>
-void gemmBinary_neon_tile2x2x2(uint64_t * A, uint64_t * BT, AccType * CT, AccType alpha,
+void gemmBinary_neon_tile4x2x2(uint64_t * A, uint64_t * BT, AccType * CT, AccType alpha,
 uint64_t rowsA, uint64_t depth_words, uint64_t rowsBT) {
-  assert(rowsA % 2 == 0);
-  assert(rowsBT % 2 == 0);
-  assert(depth_words % 2 == 0);
+  const uint64_t Atile = 4, BTtile = 2, DepthTile = 2;
+  const size_t num_acc = Atile*BTtile;
+  assert(rowsA % Atile == 0);
+  assert(depth_words % DepthTile == 0);
+  assert(rowsBT % BTTile == 0);
 
-  for(uint64_t rBT = 0; rBT < rowsBT; rBT+=2) {
+  for(uint64_t rBT = 0; rBT < rowsBT; rBT += BTtile) {
     uint64_t * BTptr = &BT[rBT * depth_words];
-    for(uint64_t rA = 0; rA < rowsA; rA+=2) {
+    for(uint64_t rA = 0; rA < rowsA; rA += Atile) {
       uint64_t * Aptr = &A[rA * depth_words];
-      uint64_t acc[4] = {0};
-      uint8x16_t acc_neon[4];
+      uint64_t acc[num_acc] = {0};
+      uint8x16_t acc_neon[num_acc];
       // TODO could keep things in 16-bit accumulators for perf.
-      uint64x2_t acc2_neon[4];
+      uint64x2_t acc2_neon[num_acc];
       // initialize accumulators to zero
-      for(int init = 0; init < 4; init++) {
+      for(size_t init = 0; init < num_acc; init++) {
         acc_neon[init] = vcombine_u8(vcreate_u8(0), vcreate_u8(0));
         acc2_neon[init] = vcombine_u64(vcreate_u64(0), vcreate_u64(0));
       }
 
-      for(uint64_t d = 0; d < depth_words; d+=2) {
-        uint8x16_t a0, b0, a1, b1;
-        a0 = vld1q_u8((uint8_t *) &Aptr[d]);
-        a1 = vld1q_u8((uint8_t *) &Aptr[d + depth_words]);
+      for(uint64_t d = 0; d < depth_words; d += DepthTile) {
+        uint8x16_t a0, b0, a1, b1, a2, a3;
+
+        a0 = vld1q_u8((uint8_t *) &Aptr[d + 0*depth_words]);
+        a1 = vld1q_u8((uint8_t *) &Aptr[d + 1*depth_words]);
+        a2 = vld1q_u8((uint8_t *) &Aptr[d + 2*depth_words]);
+        a3 = vld1q_u8((uint8_t *) &Aptr[d + 3*depth_words]);
         b0 = vld1q_u8((uint8_t *) &BTptr[d]);
         b1 = vld1q_u8((uint8_t *) &BTptr[d + depth_words]);
 
@@ -35,27 +42,32 @@ uint64_t rowsA, uint64_t depth_words, uint64_t rowsBT) {
         acc_neon[1] = vaddq_u8(acc_neon[1], vcntq_u8(vandq_u8(a0, b1)));
         acc_neon[2] = vaddq_u8(acc_neon[2], vcntq_u8(vandq_u8(a1, b0)));
         acc_neon[3] = vaddq_u8(acc_neon[3], vcntq_u8(vandq_u8(a1, b1)));
+        acc_neon[4] = vaddq_u8(acc_neon[4], vcntq_u8(vandq_u8(a2, b0)));
+        acc_neon[5] = vaddq_u8(acc_neon[5], vcntq_u8(vandq_u8(a2, b1)));
+        acc_neon[6] = vaddq_u8(acc_neon[6], vcntq_u8(vandq_u8(a3, b0)));
+        acc_neon[7] = vaddq_u8(acc_neon[7], vcntq_u8(vandq_u8(a3, b1)));
 
-        if((d & 15L) == 0L) {
-          /* hsum over 8-bit accumulators when end or overflow*/
-          for(int init = 0; init < 4; init++) {
+        if((d & 7L) == 7L) {
+          // hsum over 8-bit accumulators when end or overflow
+          for(size_t init = 0; init < num_acc; init++) {
             acc2_neon[init] = vaddq_u64(acc2_neon[init], vpaddlq_u32(vpaddlq_u16(vpaddlq_u8(acc_neon[init]))));
             acc_neon[init] = vcombine_u8(vcreate_u8(0), vcreate_u8(0));
           }
         }
       }
       /* move into regular accumulators */
-      for(int init = 0; init < 4; init++) {
+      for(size_t init = 0; init < num_acc; init++) {
         uint64_t tmp[2];
         acc2_neon[init] = vaddq_u64(acc2_neon[init], vpaddlq_u32(vpaddlq_u16(vpaddlq_u8(acc_neon[init]))));
         vst1q_u64(tmp, acc2_neon[init]);
         acc[init] = tmp[0] + tmp[1];
       }
 
-      CT[rBT * rowsA + rA] += acc[0] * alpha;
-      CT[(rBT + 1) * rowsA + rA] += acc[1] * alpha;
-      CT[rBT * rowsA + rA + 1] += acc[2] * alpha;
-      CT[(rBT + 1) * rowsA + rA + 1] += acc[3] * alpha;
+      for(uint64_t at = 0; at < Atile; at++) {
+        for(uint64_t bt = 0; bt < BTtile; bt++) {
+          CT[(rBT + bt) * rowsA + (rA + at)] += acc[at * BTtile + bt] * alpha;
+        }
+      }
     }
   }
 }
@@ -79,8 +91,8 @@ void gemmBitSerial_neon_usingBinary(BitSerialMatrix * lhs, BitSerialMatrix * rhs
       bool neg_rhs = rhs->issigned && (rbit == rhsbits-1);
       bool neg = neg_rhs ^ neg_lhs;
       AccType alpha = neg ? -(1 << (lbit+rbit)) : (1 << (lbit+rbit));
-      if(lhs->nrows % 2 == 0 && rhs->nrows % 2 == 0 && lhs->wordsPerRow() % 2 == 0) {
-        gemmBinary_neon_tile2x2x2(lhs->bitplaneptr(lbit), rhs->bitplaneptr(rbit), res, alpha, lhs->nrows, lhs->wordsPerRow(), rhs->nrows);
+      if(lhs->nrows % 4 == 0 && lhs->wordsPerRow() % 2 == 0 && rhs->nrows % 2 == 0) {
+        gemmBinary_neon_tile4x2x2(lhs->bitplaneptr(lbit), rhs->bitplaneptr(rbit), res, alpha, lhs->nrows, lhs->wordsPerRow(), rhs->nrows);
       }
       else {
         gemmBinary_generic_naive(lhs->bitplaneptr(lbit), rhs->bitplaneptr(rbit), res, alpha, lhs->nrows, lhs->wordsPerRow(), rhs->nrows);
--- jni/Application.mk
+++ jni/Application.mk
@@ -1,5 +1,5 @@
 APP_STL := c++_static
 APP_ABI := arm64-v8a
-APP_CPPFLAGS := -O3 -std=c++11 -Wall -Wextra -pedantic -Wno-unused-variable -Wno-unused-parameter -I.
+APP_CPPFLAGS := -O3 -funroll-loops -std=c++11 -Wall -Wextra -pedantic -Wno-unused-variable -Wno-unused-parameter -I.
 APP_LDFLAGS := -L$(SYSROOT)/usr/lib -lstdc++ -latomic
 APP_PIE := true
