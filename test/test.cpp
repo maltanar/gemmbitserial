a9223b7c01d5301c24f256fcc3f89ef06397ef4e
Yaman Umuroglu <maltanar@gmail.com>
2018-01-30 22:30:37 +0100

    separate test helpers in own file

--- test/test.cpp
+++ test/test.cpp
@@ -6,6 +6,7 @@
 #include <deque>
 #include "gemmbitserial.hpp"
 #include "mnistdata.h"
+#include "testhelpers.hpp"
 
 using namespace std;
 using namespace gemmbitserial;
@@ -13,88 +14,15 @@ using namespace gemmbitserial;
 #define VERBOSE_TEST(x) ;
 //#define VERBOSE_TEST(x) x
 
-// Generate a random vector of -1 and +1 values of given dimension
-template <typename T>
-void generateRandomVector_Bipolar(size_t dim, T * ret) {
-  for(size_t i = 0; i < dim; i++) {
-    ret[i] = (rand() % 2 == 0) ? 1 : -1;
-  }
-}
-
-/**
-* Generate a random vector with given dimension and number of bits
-*/
-template <typename T>
-void generateRandomVector(size_t bits, size_t dim, T * ret, bool allowNeg = false) {
-  assert(bits <= (sizeof(T) * 8));
-  if(bits == 1 && allowNeg) {
-    // generate bipolar values
-    generateRandomVector_Bipolar(dim, ret);
-    return;
-  }
-  int32_t minVal = 0;
-  int32_t maxVal = (1 << bits);
-  for(size_t i = 0; i < dim; i++) {
-    ret[i] = (rand() % maxVal) - (allowNeg ? maxVal/2 : 0);
-  }
-}
-
-template <typename LHSType, typename RHSType>
-void naive_int_gemm(LHSType * lhs, RHSType * rhs, int32_t * res, int rows, int depth, int cols) {
-  for(int k = 0; k < cols; k++) {
-    for(int i = 0; i < rows; i++) {
-      int32_t acc = 0;
-      for(int j = 0; j < depth; j++) {
-        acc += lhs[i * depth + j] * rhs[k * depth + j];
-      }
-      res[k * rows + i] = acc;
-    }
-  }
-}
-
-template <typename T>
-void naive_sum_rows(T * m, int32_t * res, int rows, int cols) {
-  for(int i = 0; i < rows; i++) {
-    int32_t acc = 0;
-    for(int k = 0; k < cols; k++) {
-      acc += m[i * cols + k];
-    }
-    res[i] = acc;
-  }
-}
-
-template <typename T>
-void printmatrix(T * mat, int rows, int cols) {
-  for(int i = 0; i < rows; i++) {
-    for(int j = 0; j < cols; j++) {
-      cout << (int) mat[i * cols + j] << " ";
-    }
-    cout << endl;
-  }
-  cout << endl;
-}
-
-template <typename T>
-void printmatrixdiff(const T * mat1, const T * mat2, int rows, int cols) {
-  for(int i = 0; i < rows; i++) {
-    for(int j = 0; j < cols; j++) {
-      if(mat1[i * cols + j] != mat2[i * cols + j]) {
-        cout << "Difference at (i,j) = " << i << " " << j << " Mat1: " << (int)mat1[i * cols + j] << " Mat2: " << mat2[i * cols + j] << endl;
-      }
-    }
-  }
-  cout << endl;
-}
-
 void printBitSerialMatrix(BitSerialMatrix * bsm) {
-  cout << "BitSerialMatrix with bits " << bsm->nbits << " rows " << bsm->nrows << " cols " << bsm->ncols << endl;
+  std::cout << "BitSerialMatrix with bits " << bsm->nbits << " rows " << bsm->nrows << " cols " << bsm->ncols << std::endl;
   for(int b = 0; b < bsm->nbits; b++) {
-    cout << "bit " << b << ":" << endl;
+    std::cout << "bit " << b << ":" << std::endl;
     for(int r = 0; r < bsm->nrows; r++) {
       for(int c = 0; c < bsm->ncols; c++) {
-        cout << (bsm->get(b,r,c) ? 1 : 0) << " ";
+        std::cout << (bsm->get(b,r,c) ? 1 : 0) << " ";
       }
-      cout << endl << endl;
+      std::cout << std::endl << std::endl;
     }
   }
 }
